a4cf1e19bc0c2fed32a9ac38a07bd25be69e9ef6
IshikaSahu441 <ishika.sahu22@st.niituniversity.in>
2025-08-14 22:02:26 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    naem = "medical_chatbot",
+    version = "0.1.0",
+    author = "Ishika Sahu",
+    packages = find_packages(),
+    install_requires = []
+)
