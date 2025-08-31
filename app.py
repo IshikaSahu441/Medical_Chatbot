c1527de514adcbebc285670cbaa409704c8eb904
IshikaSahu441 <ishika.sahu22@st.niituniversity.in>
2025-08-31 20:09:15 +0530

    port changed

--- app.py
+++ app.py
@@ -61,5 +61,5 @@ def chat():
 
 if __name__ == '__main__':
     # app.run(host = "0.0.0.0", port = 3000, debug = True)
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
